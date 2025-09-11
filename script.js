f1c02f0e48d5135c0f64159de902e2981e7c0928
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-11 20:16:36 -0300

    condicionais.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let nome ="Isabely";
-let idade = 17;
-let altura = 1.59;
-let estudante = true;
-let endereco = "Rua dos atiradores, 8805";
+/*Em portugol: 
+ inteiro nota = 85;
 
-console.log(`Nome: ${nome}`);
-console.log(`Idade: ${idade}`);
-console.log(`Altura: ${altura}m`);
-console.log(`Estudante: ${estudante}`);
-console.log(`Endereço: ${endereco}`);
+ se (nota >= 90){
+  escreva("Aprovado");
+ } senao {
+  escreva("Reprovado")
+ } */
+
+
+let nota = 85;
+
+if (nota >= 90) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
