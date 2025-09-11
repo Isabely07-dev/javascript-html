e65ebda1e111fdb4affd18bc487ee4da37b52bcf
Isabely07-dev <isabely_reitz@estudante.sesisenai.org.br>
2025-09-11 19:23:18 -0300

    Variáveis

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = "Isabely";
+let idade = 17;
+let cidade ="Pomerode - SC";
+let altura = 1.59;
+let ehHumano = true;
+
+console.log("Nome:" + nome);
+console.log("Idade:" + idade);
+console.log("Cidade:" + cidade);
+console.log("Altura:" + altura + m);
+console.log("É humano?" + ehHumano);
+
+
